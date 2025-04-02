7b753879426e9a2485f1507fa0894a86e20f884b
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-02 19:41:03 +1100

    2

--- frontend/src/jobs.js
+++ frontend/src/jobs.js
@@ -58,3 +58,57 @@ export const populateFeed = () => {
             populateFeedLock = false;
         });
 };
+
+
+export const populatePostCards = (data, containerId) => {
+    console.log('data', data)
+    
+    const cardPromises = data.map((item) => {
+        const feedDom = document.createElement("div");
+        feedDom.className = "card mb-3 feed-card";
+
+        const row = document.createElement("div");
+        row.className = "row no-gutters";
+        feedDom.appendChild(row);
+
+        const colImg = document.createElement("div");
+        colImg.className = "col-md-3";
+        row.appendChild(colImg);
+
+        const imgWrapper = document.createElement("div");
+        imgWrapper.className = "card-img img-wrapper";
+        colImg.appendChild(imgWrapper);
+        const img = document.createElement("img");
+        img.src = item.image;
+        img.alt = `Job Image for ${item.title}`;
+        img.className = "job-image";
+        img.alt = `Image of ${item.title}`;
+        imgWrapper.appendChild(img);
+
+        const colBody = document.createElement("div");
+        colBody.className = "col-md-9";
+        row.appendChild(colBody);
+
+        const cardBody = document.createElement("div");
+        cardBody.className = "card-body";
+        colBody.appendChild(cardBody);
+
+        const title = document.createElement("h5");
+        title.className = "card-title";
+        title.textContent = item.title;
+        cardBody.appendChild(title);
+
+        const description = document.createElement("p");
+        description.className = "card-text";
+        description.textContent = item.description;
+        cardBody.appendChild(description);
+
+        const extraInfo = document.createElement("div");
+        extraInfo.className = "creator-time-wrapper";
+        cardBody.appendChild(extraInfo);
+
+        const creatorTextPromise = getCreatorUsername(item.creatorId)
+            .then((creatorName) => {
+                const creatorText = createInfoTextElement("Posted by: " + creatorName, "card-text text-muted post-creator-text");
+                return creatorText;
+            })
