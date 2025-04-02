c116438624be45f3eebcb11842ba8dba4ca5253d
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-02 20:04:14 +1100

    2

--- frontend/src/users.js
+++ frontend/src/users.js
@@ -50,4 +50,74 @@ export const populateUserInfo = (userId) => {
 };
 
 
+export const populateWatchees = (data) => {
+    const watchees = data.usersWhoWatchMeUserIds;
+    const numWatchees = watchees.length;
+
+    
+    const numWatcheesElement = document.getElementById("watchees-num");
+    numWatcheesElement.textContent = numWatchees;
+    numWatcheesElement.style.fontWeight = "bold";
+
+    const watcheesContainer = document.getElementById("user-watchees");
+    watcheesContainer.textContent = "";
+    for (const watcheeId of watchees) {
+        const payload = {
+            userId: watcheeId,
+        };
+
+        const watcheeElement = document.createElement("div");
+        watcheeElement.classList.add("card", "mb-3");
+        watcheeElement.style.width = "180px";
+        watcheeElement.style.height = "90px";
+        watcheeElement.style.margin = "10px";
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body", "p-2");
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title", "mb-1");
+
+        const cardSubtitle = document.createElement("h6");
+        cardSubtitle.classList.add("card-subtitle", "text-muted", "mb-1");
+        apiCall("user", "GET", payload)
+            .then((watcheeInfo) => {
+                cardSubtitle.textContent = watcheeInfo.email;
+                cardTitle.textContent = watcheeInfo.name;
+            });
+        const cardButton = document.createElement("button");
+        cardButton.classList.add("btn",  "btn-sm");
+        cardButton.style.width = "4em";
+        cardButton.style.textAlign = "right";
+        cardButton.style.position = "absolute";
+        cardButton.style.right = "0";
+        cardButton.style.bottom = "0";
+        cardButton.style.marginLeft = "10px";
+        cardButton.textContent = "View";
+        cardButton.setAttribute("id", "watchee-card-button");
+        cardButton.setAttribute("value", `${watcheeId}`);
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardSubtitle);
+        watcheeElement.appendChild(cardButton);
+        watcheeElement.appendChild(cardBody);
+        watcheesContainer.appendChild(watcheeElement);
+
+        cardButton.addEventListener("click", () => {
+            
+            populateUserInfo(watcheeId)
+                .then((data) => {
+                    document.getElementById("user-jobs").textContent = "";
+                    populatePostCards(data.jobs, "user-jobs");
+                    populateWatchees(data);
+                });
+        });
+    }
+};
+
+
+document.getElementById("nav-profile").addEventListener("click", () => {
+    hide("section-logged-in");
+    hide("main-content");
+    show("page-profile");
 
