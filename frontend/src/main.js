d86c8f0ffc198be0dbfeb71a57ebfabdc2ffc573
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-02 19:25:14 +1100

    7

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,11 @@
+import { handleLoginUI } from "./helpers.js";
+import { populateFeed } from "./jobs.js";
+import "./dropZone.js";
+import "./router.js";
 
+
+if (localStorage.getItem("token")) {
+    handleLoginUI();
+    populateFeed();
+}
 
